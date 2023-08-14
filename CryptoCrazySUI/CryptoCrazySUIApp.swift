28ac3f13778aeaa807ef283fb2409c281d5c3795
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-08-14 13:20:58 +0300

    Initial Commit

---
+++ CryptoCrazySUI/CryptoCrazySUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySUIApp.swift
+//  CryptoCrazySUI
+//
+//  Created by Beste Kocaoglu on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
